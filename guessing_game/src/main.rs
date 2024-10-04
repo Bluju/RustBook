13f6d140eb99e8bada8a6fe693c6c08852c65f4f
bluju <43455717+Bluju@users.noreply.github.com>
2024-10-04 15:50:10 -0500

    Part 1 input from keyboard and print

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io; // input output library from standard library
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new(); // Guess is a mutable string variable bound to an empty instance of a String
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // expect will crash the program with unexpected input
+
+    println!("You guessed: {}", guess);
 }
---
+++ hello_cargo/target/debug/deps/hello_cargo-3058ad76c0b1fe37.d
@@ -0,0 +1,5 @@
+C:\Projects\RustBook\hello_cargo\target\debug\deps\libhello_cargo-3058ad76c0b1fe37.rmeta: src/main.rs
+
+C:\Projects\RustBook\hello_cargo\target\debug\deps\hello_cargo-3058ad76c0b1fe37.d: src/main.rs
+
+src/main.rs:
